84290ff0c612d144298e53d2649739e7c9152ae7
YakakaHM <yhmustapha22@gmail.com>
2023-04-23 18:33:06 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,20 +14,20 @@ int main(void)
 
 	int d, p, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = '0'; d < '8'; d++)
 	{
 
-	for (p = d + 1; p <= '9'; p++)
+	for (p = d + 1; p < '9'; p++)
 	{
 
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
 	{
-	putchar(d);
-	putchar(p);
-	putchar(q);
-	if (d == '7' && p == '8')
+	putchar((d % 10) + '0');
+	putchar((p % 10) + '0');
+	putchar((q % 10) + '0');
+	if (d == '7' && p == '8' && q == '9')
 	putchar(',');
 	putchar(' ');
 	}
